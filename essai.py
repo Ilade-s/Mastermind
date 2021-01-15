afb2172d961bf716dce0bd4e072abe8b8647da21
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2021-01-15 16:25:32 +0100

    essai et début main

--- essai.py
+++ essai.py
@@ -1,5 +1,7 @@
 # init.py
-def essai():
+# Pas utilisée car inutile
+def essai(Input : int) -> list:
+  """append la string de la couleur dans la liste combiUser puis la retourne"""
   pass
   
 if __name__ == "__main__": # test
--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 # main.py
-a = 1
+# Texte d'explication
+from essai import *
+
+
+print("Bienvenue sur mon jeu de Mastermind !")
+print("Vous avez 10 essais pour trouver une combinaison de 4 chiffres/couleurs de 0 à 8 inclus")
+print("A chaque essai, il vous faudra entrer les quatres chiffres (de 0 à 8) que vous pensez les bons")
+print("Après chaque essai, une liste de noir (N) et blanc (B) vous sera affiché : ")
+print("\t- Blanc (B) indique une bonne valeurs/couleurs mais mal placée dans la combinaison")
+print("\t- Noir (N) indique une valeur correcte et bien placée")
+print("\a") ; print("\t\tAmusez-vous bien !")
+input("Entrée pour commencer le jeu...")
+# programme
+nEssais = 0
+while nEssais<10:
+    nEssais+=1
+    CombinaisonUser = []
+    for ninput in range(4):
+        CombinaisonUser.append(int(input("Input couleur/valeur "+str(ninput+1)+" : ")))
+    print("Combinaison choisie :",CombinaisonUser)
+    
