052e96e179e1563058289fe935e4c09686933d24
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2021-01-16 18:19:50 +0100

    ajout introduction

--- main.py
+++ main.py
@@ -2,15 +2,25 @@
 # Texte d'explication
 from init import *
 from test import *
-
-print("Bienvenue sur mon jeu de Mastermind !")
-print("Vous avez 10 essais pour trouver une combinaison de 4 chiffres/couleurs de 0 à 8 inclus")
-print("A chaque essai, il vous faudra entrer les quatres chiffres (de 0 à 8) que vous pensez les bons")
-print("Après chaque essai, une liste de noir (N) et blanc (B) vous sera affiché : ")
-print("\t- Blanc (B) indique une bonne valeurs/couleurs mais mal placée dans la combinaison")
-print("\t- Noir (N) indique une valeur correcte et bien placée")
-print("\a") ; print("\t\tAmusez-vous bien !")
-input("Entrée pour commencer le jeu...")
+from tkinter import *
+root = Tk()
+label = Label(root, text="Bienvenue sur mon jeu de Mastermind !")
+label.pack(padx=5,pady=5)
+label = Label(root, text="Vous avez 10 essais pour trouver une combinaison de 4 chiffres/couleurs de 0 à 8 inclus")
+label.pack(padx=5,pady=5)
+label = Label(root, text="A chaque essai, il vous faudra entrer les quatres chiffres (de 0 à 8) que vous pensez les bons")
+label.pack(padx=5,pady=5)
+label = Label(root, text="Après chaque essai, une liste de noir (N) et blanc (B) vous sera affiché : ")
+label.pack(padx=5,pady=5)
+label = Label(root, text="- Blanc (B) indique une bonne valeurs/couleurs mais mal placée dans la combinaison")
+label.pack(padx=5,pady=5)
+label = Label(root, text="- Noir (N) indique une valeur correcte et bien placée")
+label.pack(padx=5,pady=5)
+label = Label(root, text="Amusez-vous bien !")
+label.pack(padx=5,pady=5)
+button = Button(root, text="Lancer le jeu", command=root.destroy)
+button.pack(pady=10,padx=10)
+root.mainloop()
 # programme
 nEssais = 0
 CombinaisonATrouver = init()
