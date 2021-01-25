8e5f985d1e1429cfb1699bbc77d7c78beaf088a0
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2021-01-25 08:44:30 +0100

    ajout classes

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 # main.py
+import tkinter
 from init import *
 from test import *
 from essai import *
 from tkinter import * # GUI
 from ToolsPerso import * # outils
+class Affichages(tkinter):
+    """Affichages tkinter"""
+    def Principale(root):
+        """Fenêtre affichant la combinasion choisie et le résulata précédent, et permet à l'utilsateur de valider son choix"""
+        for i in range(4):
+            root.rowconfigure(i,weight=1)
+        for j in range(4):
+            root.columnconfigure(j,weight=1)
+        label = Label(root, text="\/Résultat de l'essai précédent (gris et valeur -1 si vide)\/", font=(10))
+        label.grid(row=0,column=0,columnspan=4)
+
+        # Affichage combinaison user
+        for i in range(len(CombinaisonUser)):
+            valClr = CombinaisonUser[i]
+            label = Label(root, text=valClr, bg=couleurs[valClr], relief=SOLID, font=(8))
+            label.grid(row=2, column=i,ipadx=75,ipady=75)
+
+        # Affichage combinaison noir et blancs
+        for i in range(len(ResultatEssai)):
+            valClr = ResultatEssai[i]
+            label = Label(root, text=valClr, bg=couleurs[valClr], relief=SOLID, foreground="red", font=(8))
+            label.grid(row=1, column=i,ipadx=75,ipady=75)
+
+        # Dernière ligne
+        label = Label(root, text="/\Combinaison choisie/\\", font=(10))
+        label.grid(row=3,column=1,columnspan=2)
+        button = Button(root, text="Valider", command=root.destroy, bg="#6bc55d", activebackground="grey") # bouton vert 
+        button.grid(row=3,column=0,ipadx=50,ipady=30)
+        button = Button(root, text="Annuler", command=root.destroy, bg="#db2915", activebackground="grey") # bouton rouge
+        button.grid(row=3,column=3,ipadx=50,ipady=30)
+        root.mainloop()
+    def Secondaire(root):
+        pass
 # Texte d'explication (introduction)
 root = Tk()
 root.title("Introduction")
@@ -25,29 +59,17 @@ button = Button(root, text="Lancer le jeu", command=root.destroy)
 button.pack(pady=10,padx=10)
 root.mainloop()
 # programme
-couleurs = {0:""}
+CombinaisonUser = [-1,-1,-1,-1]
+ResultatEssai = [-1,-1,-1,-1]
 CombinaisonATrouver = init()
+couleurs = {-1:"grey","B":"white","N":"black"}
 root = Tk()
 root.title("Mastermind")
-root.geometry("500x500")
-for i in range(4):
-    root.rowconfigure(i,weight=1)
-for j in range(4):
-    root.columnconfigure(j,weight=1)
-label = Label(root, text="djfkjdsfjosdfjosdfjsdfdsigfh", font=(10))
-label.grid(row=0,column=0,columnspan=4)
-for l in range(2,4):
-    for i in CombinaisonATrouver:
-        label = Label(root, text="", bg="red")
-button = Button(root, text="Valider", command=root.destroy, bg="#6bc55d", activebackground="grey") # bouton vert 
-button.grid(row=3,column=1,ipadx=20,ipady=20)
-button = Button(root, text="Annuler", command=root.destroy, bg="#db2915", activebackground="grey") # bouton rouge
-button.grid(row=3,column=2,ipadx=20,ipady=20)
-root.mainloop()
+root.geometry("600x600")
+Affichages.Principale(root) # Affichage première fenêtre
 # programme
 nEssais = 0
 CombinaisonATrouver = init()
-CombinaisonUser = []
 while nEssais<10 and not CombinaisonUser==CombinaisonATrouver: # Boucle jeu
     nEssais+=1
     # print(CombinaisonATrouver) # debug victoire
