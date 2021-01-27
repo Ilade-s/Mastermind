75a2f9ba039d5c9ba7dad924d3806364b47aa946
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2021-01-27 08:43:51 +0100

    changes

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ class Affichages:
     """Affichages tkinter"""
     def __init__(self) -> None:
         self.CombinaisonUser = []
-        self.ResultatEssai = []
-    def Intro():
+        self.ResultatEssai = [-1,-1,-1,-1]
+    def Intro(self):
         """Fenêtre avec texte d'introduction"""
         root.title("Introduction")
         label = Label(root, text="Bienvenue sur mon jeu de Mastermind !")
@@ -57,7 +57,8 @@ class Affichages:
         button = Button(root, text="Lancer le jeu", command=root.destroy)
         button.pack(pady=10,padx=10)
         root.mainloop()
-    def Principale():
+    def Principale(self):
+        nEssais
         def ButtonCancel():
             """Réinitialisation par clic bouton annuler"""
             Affichage.CombinaisonUser = [-1,-1,-1,-1]
@@ -71,16 +72,16 @@ class Affichages:
                 else: # Défaite
                     Affichage.EcranDefaite()
             else: # Partie pas terminée
-                Affichage.CombinaisonUser = [-1,-1,-1,-1]
+                nEssais+=1
+                Affichage.ResultatEssai = CompleterResEssai(test(Affichage.CombinaisonUser, CombinaisonATrouver))
                 Affichage.Principale() # Mise à jour
         """Fenêtre affichant la combinaison choisie et le résultat précédent, et permet à l'utilsateur de valider son choix"""
-        ResultatEssai = CompleterResEssai(test(Affichage.CombinaisonUser, CombinaisonATrouver))
         root.title("Mastermind : fenêtre principale")
         for i in range(4):
             root.rowconfigure(i,weight=1)
         for j in range(4):
             root.columnconfigure(j,weight=1)
-        label = Label(root, text="\/Résultat de l'essai précédent (gris et valeur -1 si vide)\/", font=(10))
+        label = Label(root, text="Il vous reste "+str(10-nEssais)+" essais restants", font=(10))
         label.grid(row=0,column=0,columnspan=4)
         # Affichage combinaison user
         for i in range(len(Affichage.CombinaisonUser)):
@@ -88,8 +89,8 @@ class Affichages:
             label = Label(root, text=valClr, bg=couleurs[valClr], relief=RAISED, font=(8))
             label.grid(row=2, column=i,ipadx=75,ipady=75)
         # Affichage combinaison noir et blancs
-        for i in range(len(ResultatEssai)):
-            valClr = ResultatEssai[i]
+        for i in range(len(self.ResultatEssai)):
+            valClr = self.ResultatEssai[i]
             label = Label(root, text=valClr, bg=couleurs[valClr], relief=RAISED, foreground="red", font=(8))
             label.grid(row=1, column=i,ipadx=75,ipady=75)
         # Dernière ligne
@@ -99,7 +100,7 @@ class Affichages:
         button.grid(row=3,column=0,ipadx=50,ipady=30)
         button = Button(root, text="Annuler", command=ButtonCancel, bg="#db2915", activebackground="grey") # bouton rouge
         button.grid(row=3,column=3,ipadx=50,ipady=30)
-    def Secondaire():
+    def Secondaire(self):
         def ClicClr(clr):
             """Ajoute la couleur choisie (selon le bouton) à CombUser si possible et met à jour les textes"""
             if len(CombUser)<4:    
@@ -131,7 +132,7 @@ class Affichages:
         button.grid(row=4, column=0,ipadx=50,ipady=30)
         button = Button(root, text="Annuler", bg="#db2915", command=partial(TransitionAffichages, False, None),font=(10))
         button.grid(row=4, column=2,ipadx=50,ipady=30)
-    def EcranVictoire():
+    def EcranVictoire(self):
         """Ecran de victoire à afficher"""
         for w in root.winfo_children():
             w.destroy()
@@ -159,11 +160,10 @@ class Affichages:
         pass
 # Texte d'explication (introduction)
 root = Tk()
-Affichage = Affichages
+Affichage = Affichages()
 Affichage.Intro()
 # programme
 Affichage.CombinaisonUser = [-1,-1,-1,-1]
-ResultatEssai = [-1,-1,-1,-1]
 CombinaisonATrouver = init()
 print(CombinaisonATrouver) # debug
 couleurs = {-1:"grey","B":"white","N":"black"\
